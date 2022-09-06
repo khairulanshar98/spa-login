603cd01622306df73f9f3dc4f69ffb06885d8951
Khairul Anshar <halo@khairulanshar.com>
2022-09-06 11:25:51 +0800

    button

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,9 @@
+// @ts-ignore
+import { Button } from "@khairul/nav";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (<>
+    <section>{props.name} is mounted!</section>
+    <Button.default module='Login' />
+  </>)
 }
